19e0fcea25850722331fac38483f2ddc13607698
helloitsjoe <jboyle617@gmail.com>
2022-04-18 15:41:09 -0400

    Monster names

--- src/player.rs
+++ src/player.rs
@@ -1,15 +1,19 @@
-use super::{Map, Player, Position, State, TileType, Viewshed};
-use rltk::{Rltk, VirtualKeyCode};
+use super::{Map, Player, Position, RunState, State, TileType, Viewshed};
+use rltk::{Point, Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let mut positions = ecs.write_storage::<Position>();
     let mut viewsheds = ecs.write_storage::<Viewshed>();
     let map = ecs.fetch::<Map>();
 
     for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+        let mut ppos = ecs.write_resource::<Point>();
+        ppos.x = pos.x;
+        ppos.y = pos.y;
+
         let new_x = pos.x + delta_x;
         let new_y = pos.y + delta_y;
         let destination_idx = map.xy_idx(new_x, new_y);
@@ -22,10 +26,10 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     }
 }
 
-pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
     // Player movement
     match ctx.key {
-        None => {}
+        None => return RunState::Paused,
         Some(key) => match key {
             VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
                 try_move_player(-1, 0, &mut gs.ecs)
@@ -39,7 +43,8 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
             VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
                 try_move_player(0, 1, &mut gs.ecs)
             }
-            _ => {}
+            _ => return RunState::Paused,
         },
     }
+    RunState::Running
 }
